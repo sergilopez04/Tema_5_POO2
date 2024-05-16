4292f42b6b8ce2ed612be6d96c45cd388c74daf7
[Sergi López] <[socelsergi@gmail.com]>
2024-05-16 10:59:01 +0200

    ex1 done

---
+++ Ex1.php
@@ -0,0 +1,36 @@
+<?php
+abstract class Animal{
+    protected $name;
+
+    public function __construct(string $name)
+    {
+        $this->name = $name;
+    }
+    public function getName(){
+        return $this->name;
+    }
+
+    public function makeSound(){}
+}
+
+class Dog extends Animal{
+    public function makeSound()
+    {
+        return "Bup, bup";
+    }
+}
+
+class Cat extends Animal{
+    public function makeSound()
+    {
+        return "Meuuu";
+    }
+}
+
+$cat = new Cat("Mustikad");
+$dog = new  Dog("Aika");
+
+echo "Em dic " . $cat->getName() . ", i faig " . $cat->makeSound().PHP_EOL;
+echo "Em dic " . $dog->getName() . ", i faig " . $dog->makeSound().PHP_EOL;
+
+?>
